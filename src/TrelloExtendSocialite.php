66d3919432c42e4e598025615538b0da33a13953
DraperStudio <hello@draperstud.io>
2015-02-25 11:54:46 +0100

    Big clean up

--- src/TrelloExtendSocialite.php
+++ src/TrelloExtendSocialite.php
@@ -5,11 +5,13 @@ use SocialiteProviders\Manager\SocialiteWasCalled;
 
 class TrelloExtendSocialite
 {
+    /**
+     * Execute the provider.
+     */
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
         $socialiteWasCalled->extendSocialite(
-            'trello',
-            __NAMESPACE__.'\Provider',
+            'trello', __NAMESPACE__.'\Provider',
             'League\OAuth1\Client\Server\Trello'
         );
     }
